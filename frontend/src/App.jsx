109b33e99f14961b4eac5b58e76b4af2084e74ba
Erick Rodrigues <149729630+erickki@users.noreply.github.com>
2025-10-09 21:06:30 -0300

    Update
    09/10/2025 - 21:06

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,3 @@
 export default function App() {
-  return (
-    <div>
-      <div className="animate-fade-up duration-700">
-        Teste animação funcionando ✨
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
